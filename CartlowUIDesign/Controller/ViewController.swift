bd76b00454cbdc63847ae123b17e14aac5cc8486
fadirasheed006 <fadirasheed006@gmail.com>
2020-09-14 19:50:53 +0500

    initial Commit

---
+++ CartlowUIDesign/Controller/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  CartlowUIDesign
+//
+//  Created by macbook on 14/09/2020.
+//  Copyright © 2020 macbook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var categoryCollection: UICollectionView!
+    
+    var categories = [Category]()
+    var homeApi = HomeResponseApi()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        categoryCollection.dataSource = self
+        
+        homeApi.getHomeResponse { (homeData) in
+            if let homeData = homeData {
+                self.categories = homeData.category!
+                print(self.categories.count)
+                // reload data from api and set to collection
+                DispatchQueue.main.async {
+                    self.categoryCollection.reloadData()
+                }
+                
+            }
+        }
+        
+        
+    }
+    
+
+
+}
+extension ViewController: UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        print(categories.count)
+        return categories.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = categoryCollection.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CategoryCell
+        cell.setUi(cat: categories[indexPath.row])
+        
+        return cell
+    }
+    
+    
+    
+    
+}
+
